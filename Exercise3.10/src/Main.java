3d7072a41bac9bfcb1254679c136f914f0997382
Andrew Mina <andrewmina13@gmail.com>
2023-02-15 17:28:09 -0600

    sesh1

--- Exercise3.10/src/Main.java
+++ Exercise3.10/src/Main.java
@@ -3,18 +3,19 @@ public class Main {
         HWQueue<Integer> queue = new HWQueue<>();
         Thread thread1 = new Thread(() -> {
             queue.enq(1);
+            System.out.println("thread one " + queue.tail);
 
         });
         Thread thread2 = new Thread(() -> {
-            queue.deq();
             queue.enq(2);
+            System.out.println("thread two " + queue.tail);
         });
         thread1.start();
         thread2.start();
 
-        for(int i = 0; i < queue.items.length;i++){
-            System.out.println(queue.deq());
-        }
+//        for(int i = 0; i < queue.items.length;i++){
+//            System.out.println(queue.deq());
+//        }
 
     }
 
