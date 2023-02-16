47761d1d5dc496c42affc21e26640423c9254ed3
Andrew Mina <andrewmina13@gmail.com>
2023-02-16 11:41:35 -0600

    sesh 2

--- Exercise3.10/src/Main.java
+++ Exercise3.10/src/Main.java
@@ -3,15 +3,24 @@ public class Main {
         HWQueue<Integer> queue = new HWQueue<>();
         Thread thread1 = new Thread(() -> {
             queue.enq(1);
-            System.out.println("thread one " + queue.tail);
+            queue.enq(3);
+            System.out.println("thread one dequed " + queue.deq());
 
         });
         Thread thread2 = new Thread(() -> {
             queue.enq(2);
-            System.out.println("thread two " + queue.tail);
+            queue.enq(4);
+            System.out.println("thread two dequed " + queue.deq());
         });
         thread1.start();
         thread2.start();
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
 
 //        for(int i = 0; i < queue.items.length;i++){
 //            System.out.println(queue.deq());
